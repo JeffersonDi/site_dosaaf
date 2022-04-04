4863b5e8c3714b68c10ae23a9a07bcb1a9f7c661
JeffersonDi <48020198+JeffersonDi@users.noreply.github.com>
2022-04-04 21:11:46 +0700

    Create footer.php

---
+++ blocks/footer.php
@@ -0,0 +1,11 @@
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Главная</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Сведения об образовательной организации</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">МО ДОСААФ</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Общая информация по организации</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Фотоальбомы</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Информация для поступающих</a></li>
+    </ul>
+    <p class="text-center text-muted">© <?php echo date("Y"); ?>, МО ДОСААФ России</p>
+</footer>
